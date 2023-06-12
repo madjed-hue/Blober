b36f26063b399d38d4c867b4f7d3f51cc8b4f36c
fateh beddiaf <kentounmax@gmail.com>
2023-06-12 23:34:18 +0100

    add auth providers

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import Navbar from "@/components/navbar/Navbar";
 import "./globals.css";
 import { Inter, Roboto, Poppins } from "next/font/google";
-import Footer from "@/components/footer/Footer";
+// import Footer from "@/components/footer/Footer";
 import { ThemeProvider } from "@/context/ThemeContext";
 import AuthProvider from "@/components/AuthProvider/AuthProvider";
 
@@ -22,7 +22,7 @@ export default function RootLayout({ children }) {
             <div className="container">
               <Navbar />
               {children}
-              <Footer />
+              {/* <Footer /> */}
             </div>
           </AuthProvider>
         </ThemeProvider>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -45,7 +45,7 @@ const Navbar = () => {
   return (
     <div className={styles.container}>
       <Link href="/" className={styles.logo}>
-        lamamia
+        Blobber
       </Link>
       <div className={styles.links}>
         <DarkModeToggle />
