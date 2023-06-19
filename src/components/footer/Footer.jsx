41c3f26ccc3f2fcd4ba6f559b3a837878eaadb2a
fateh beddiaf <kentounmax@gmail.com>
2023-06-19 17:37:21 +0100

    add footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import styles from "./footer.module.css";
+import Image from "next/image";
+
+const Footer = () => {
+  return (
+    <div className={styles.container}>
+      <div>©2023 Blober. All rights reserved.</div>
+      <div className={styles.social}>
+        <Image
+          src="/1.png"
+          width={15}
+          height={15}
+          className={styles.icon}
+          alt="Madjed Facebook Account"
+        />
+        <Image
+          src="/2.png"
+          width={15}
+          height={15}
+          className={styles.icon}
+          alt="Madjed Dev Instagram"
+        />
+        <Image
+          src="/3.png"
+          width={15}
+          height={15}
+          className={styles.icon}
+          alt="Madjed Dev Twitter"
+        />
+        <Image
+          src="/4.png"
+          width={15}
+          height={15}
+          className={styles.icon}
+          alt="Madjed Dev Youtube"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
