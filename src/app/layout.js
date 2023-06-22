993b62f5112241be3738e3c20c3aff76ff4f993e
fateh beddiaf <kentounmax@gmail.com>
2023-06-22 22:19:07 +0100

    add some features

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import Navbar from "@/components/navbar/Navbar";
 import "./globals.css";
 import { Inter, Roboto, Poppins } from "next/font/google";
-// import Footer from "@/components/footer/Footer";
+import Footer from "@/components/footer/Footer";
 import { ThemeProvider } from "@/context/ThemeContext";
 import AuthProvider from "@/components/AuthProvider/AuthProvider";
 
@@ -16,13 +16,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={inter.className} suppressHydrationWarning={true}>
         <ThemeProvider>
           <AuthProvider>
             <div className="container">
               <Navbar />
               {children}
-              {/* <Footer /> */}
+              <Footer />
             </div>
           </AuthProvider>
         </ThemeProvider>
